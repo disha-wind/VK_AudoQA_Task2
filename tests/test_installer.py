86a5f85ad50da63d463a81b36534b3863a40d92b
Disha <disha.wind@gmail.com>
2025-06-22 10:08:32 +0300

    Fix exe a path Add check installation directory

--- tests/test_installer.py
+++ tests/test_installer.py
@@ -1,11 +1,16 @@
 import subprocess
 import time
+from pathlib import Path
 
 import pyautogui
 
 
 def test_app_install(env_vars) -> None:
-    process = subprocess.Popen([env_vars['installer_path']])
+    installer_exe = env_vars['installer_path']
+    process = subprocess.Popen([installer_exe])
     time.sleep(2)
     pyautogui.press('enter')
     process.wait()
+
+    vkteams_exe = env_vars['vkteams_path']
+    assert Path(vkteams_exe).exists()
